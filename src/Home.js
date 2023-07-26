243ad99cc9fec755eb09165088cf53977171e4f9
sharon3snc <sharonnunez89@gmail.com>
2023-07-26 19:28:37 +0200

    ok

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 
+
 function Home() {
   return (
     <div>
@@ -9,7 +10,6 @@ function Home() {
         <h1>Yum Yum</h1>
         <nav>
           <ul>
-            <li><a href="/">Nosotros</a></li>
             <li><a href="/recetas">Recetas</a></li>
             <li><a href="/sobre-nosotros">Login</a></li>
           </ul>
@@ -19,8 +19,30 @@ function Home() {
       {/* Contenido principal de la página */}
       <main>
         <section>
-          <h2>Recetas Destacadas</h2>
-          {/* Aquí irían las tarjetas o elementos con las recetas destacadas */}
+          <div class="containerbuscador">
+              <input class="buscador" type="text" placeholder="Buscar recetas"/> 
+          </div>
+          <div class="containerfiltros">
+            <div class="filtros">
+              <select class="menus">
+                <option value="-1">Todos los menus</option>
+                <option value="1">Clásico</option>
+                <option value="7">Bajo en calorías</option>
+                <option value="2">Vegetariano</option>
+                <option value="6">Vegano</option></select>
+            </div>
+            <div class="filtros">
+              <select class="menus">
+                <option value="-1">Sin restricciones</option>
+                <option value="1">Sin gluten</option>
+                <option value="7">Sin huevo</option>
+                <option value="2">Sin lactosa</option>
+                <option value="6">Vegano</option></select>
+            </div>
+          </div>
+          <div class="rtb">
+            Planifica tus comidas de la próxima semana en minutos con un plan de comidas personalizado.
+          </div>
         </section>
       </main>
 
